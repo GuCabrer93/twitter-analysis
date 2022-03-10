83ef7b178b228cf381976a2399530b5c350f44a3
Gustavo CABRERA GODINEZ <gustavo.cabrera93@hotmail.com>
2022-03-10 16:45:08 +0100

    Connected to Tweeter

--- tweepy_test.py
+++ tweepy_test.py
@@ -1,16 +1,29 @@
 print("Hello, World!")
+
+
+#Part below works
 '''
-from tweepy import StreamingClient
-
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAORIaAEAAAAAl6GnXS7YGOeQdYa0uGwc8DMF40Q%3DLIM7DC8lSNbFmsVsgWYyJqYrl2iBCSsR3Z1uUqjJR8c2kONAG4"
-streaming_client = StreamingClient(bearer_token=bearer_token)
-
-streaming_client.
-'''
-
 from kafka import KafkaProducer
 from json import dumps
 producer = KafkaProducer(bootstrap_servers='192.168.163.130:9092', api_version=(0,10),  value_serializer=lambda m: dumps(m).encode('utf-8'))
+'''
 
 #streamlit
+
+
+from tweepy import StreamingClient
+
+class CustomInStream(StreamingClient):
+
+    def on_tweet(self, tweet):
+        print(tweet.id)
+        print(tweet.text)
+
+
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAORIaAEAAAAAl6GnXS7YGOeQdYa0uGwc8DMF40Q%3DLIM7DC8lSNbFmsVsgWYyJqYrl2iBCSsR3Z1uUqjJR8c2kONAG4"
+streaming_client = CustomInStream(bearer_token=bearer_token)
+
+streaming_client.sample()
+
+print("Hello, World!")
 
