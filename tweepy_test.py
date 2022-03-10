ce527ef0ffba87e27b7823ec970f9cd51fe292ad
Gustavo CABRERA GODINEZ <gustavo.cabrera93@hotmail.com>
2022-03-10 17:15:43 +0100

    Connected to Tweeter

--- tweepy_test.py
+++ tweepy_test.py
@@ -1,29 +1,37 @@
-print("Hello, World!")
-
-
-#Part below works
-'''
+# Imported required libraries
+from tweepy import StreamingClient
 from kafka import KafkaProducer
 from json import dumps
-producer = KafkaProducer(bootstrap_servers='192.168.163.130:9092', api_version=(0,10),  value_serializer=lambda m: dumps(m).encode('utf-8'))
-'''
-
 #streamlit
 
 
-from tweepy import StreamingClient
-
-class CustomInStream(StreamingClient):
-
-    def on_tweet(self, tweet):
-        print(tweet.id)
-        print(tweet.text)
-
-
+# Creating Constants
 bearer_token = "AAAAAAAAAAAAAAAAAAAAAORIaAEAAAAAl6GnXS7YGOeQdYa0uGwc8DMF40Q%3DLIM7DC8lSNbFmsVsgWYyJqYrl2iBCSsR3Z1uUqjJR8c2kONAG4"
-streaming_client = CustomInStream(bearer_token=bearer_token)
+kafka_home = "192.168.163.130:9092"
+kafka_version = (0,10)
 
-streaming_client.sample()
 
+
+print("Hello, World!")
+
+
+
+def read_from_stream():
+    producer = KafkaProducer(bootstrap_servers=kafka_home, api_version=kafka_version,  value_serializer=lambda m: dumps(m).encode('utf-8'))
+
+    class CustomInStream(StreamingClient):
+        def on_tweet(self, tweet):
+            print(tweet.id)
+            #print(tweet.text)
+            producer.send("my-tweets", value=tweet)
+
+    streaming_client = CustomInStream(bearer_token=bearer_token)
+    streaming_client.sample()
+
+
+def create_stream_filters():
+    print("Creating filters...")
+
+create_stream_filters()
 print("Hello, World!")
 
