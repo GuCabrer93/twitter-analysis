67559f2d8d79051d4f42975418a84db067a6be2d
Gustavo CABRERA GODINEZ <gustavo.cabrera93@hotmail.com>
2022-03-11 10:31:46 +0100

    Succeded at filtering tweets

--- tweepy_test.py
+++ tweepy_test.py
@@ -1,5 +1,5 @@
 # Imported required libraries
-from tweepy import StreamingClient
+from tweepy import StreamingClient, StreamRule
 from kafka import KafkaProducer
 from json import dumps
 #streamlit
@@ -12,26 +12,53 @@ kafka_version = (0,10)
 
 
 
-print("Hello, World!")
-
-
-
 def read_from_stream():
-    producer = KafkaProducer(bootstrap_servers=kafka_home, api_version=kafka_version,  value_serializer=lambda m: dumps(m).encode('utf-8'))
+    
 
     class CustomInStream(StreamingClient):
         def on_tweet(self, tweet):
-            print(tweet.id)
+            # Uncomment code below to test with kafka
+
+            # Please note that tweepy returns an object that needs to be serialized (i.e. converted to string) 
+            # It must be encoded using utf-8 to handle non-ascii characters
+            producer = KafkaProducer(bootstrap_servers=kafka_home, api_version=kafka_version,  value_serializer=lambda m: dumps(m).encode('utf-8'))
+            producer.send("my-tweets", value=tweet) 
+
+            #print(tweet.id)
             #print(tweet.text)
-            #producer.send("my-tweets", value=tweet)
+            #print(tweet.created_at)
 
+
+    # Creating streaming client and authenticating using bearer token
     streaming_client = CustomInStream(bearer_token=bearer_token)
+
+
+    # Creating filtering rules
+    ruleTag = "my-rule-1"
+
+    ruleValue  = ""
+    ruleValue += "lang:en"
+    ruleValue += " -is:retweet"
+    ruleValue += " -has:media"
+    ruleValue += " Ukraine"
+
+    rule1 = StreamRule(value=ruleValue, tag=ruleTag)
+
+    streaming_client.add_rules(rule1, dry_run=False)
+
+    # Adding custom fields
+    tweet_fields = []
+    tweet_fields.append("created_at")
+
+    # Start reading
+    streaming_client.filter(tweet_fields=tweet_fields)
+
+    # To test without rules or custom fields
     #streaming_client.sample()
+    
 
 
-def create_stream_filters():
-    print("Creating filters...")
 
-create_stream_filters()
-print("Hello, World!")
+# Main Program starts below
+read_from_stream()
 
